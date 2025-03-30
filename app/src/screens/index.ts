e401999dacac3d26ba5d19b841f61a25134de079
artcevvv <artcevvv@yandex.kz>
2025-03-30 12:39:59 +0500

    qwer

--- app/src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as BatchesScreen } from './BatchesScreen';
-export { default as PhenologyScreen } from './PhenologyScreen';
-export { default as AnalyticsScreen } from './AnalyticsScreen';
-export { default as LibraryScreen } from './LibraryScreen';
-export { default as NotificationsScreen } from './NotificationsScreen';
-export { ProfileScreen } from './ProfileScreen'; 
